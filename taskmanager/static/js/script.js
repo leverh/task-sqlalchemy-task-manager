6fa01b0dd44845691a9853bda679e7e07e6631c1
leverh <leverh@gmail.com>
2023-05-10 10:01:11 +0000

    Add basic HTML structure. Add tasks.html and connected via routes.py

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //sidenac initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav);
+  });
